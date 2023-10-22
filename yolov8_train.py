795b29d581072536bd4f85df0d17ba159cbc022f
unknown <masouduut94@gmail.com>
2023-10-21 17:02:25 -0700

    modified some files

--- yolov8_train.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO('yolov8s.pt')  # load a pretrained model (recommended for training)
-
-# Train the model with 2 GPUs
-results = model.train(data='data.yaml', epochs=100, imgsz=640, device=[0])
