6b47d17baa22eaeeca0fc454358dda015c913f11
masoud <masouduut94@gmail.com>
2023-12-14 05:22:18 +0330

    Modified action detection data from 6 classes into 5 classes. (replaced serves with spikes.)

--- src/ml/yolo/train-bbox.py
+++ src/ml/yolo/train-bbox.py
@@ -2,13 +2,19 @@ from ultralytics import YOLO
 
 # Load a model
 model = YOLO('/home/masoud/Desktop/projects/volleyball_analytics/weights/yolov8n.pt')  # load a pretrained.py model (recommended for training)
-
+# https://docs.ultralytics.com/usage/cfg/#predict
 # Train the model with 1 GPU.
 results = model.train(
     data='/home/masoud/Desktop/projects/volleyball_analytics/data/processed/vb_actions_6.yaml',
     epochs=200,
+    task='detect',
     batch=32,
+    optimizer='AdamW',
+    seed=1368,
+    cos_lr=True,
+    # lrf=,
+    lr0=0.0001,
+    workers=16,
     imgsz=640,
-    half=True,
     device=[0]
 )
