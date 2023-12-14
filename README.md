99a434d22cd8d656fffe43ad9a9086cdda38116f
masoud <masouduut94@gmail.com>
2023-12-14 14:03:58 +0330

    Trained action detection with 5 classes.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # volleyball_project
-This project is designed to show some how we can use deep learning methods are used to help Sports Data Analytics.
+This project is designed to use deep learning methods in Volleyball 
+Sports Data Analytics.
+
+
+
+# Todo:
+
+## ML:
+- Add more data to class spike and check the data.
+- Annotate for video classification.
+- Train video classification.
+- Use batch prediction to speed up the workflow.
+- Try Intel OpenVino...
+- Add KPIs to the workflow
+- - Setter ball distribution
+- - Spiker final ball touch on ground.
+- - Successful Receives.
+- - Receives ball distribution.
+- - Visualization of the KPIs.
+## BackEnd:
+- Add Django for DB.
+- Add RabbitMQ for backend.
+- Add video splitting to the workflow.
+
+
--- src/ml/yolo/train-bbox.py
+++ src/ml/yolo/train-bbox.py
@@ -1,18 +1,21 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO('/home/masoud/Desktop/projects/volleyball_analytics/weights/yolov8n.pt')  # load a pretrained.py model (recommended for training)
+model = YOLO('/home/masoud/Desktop/projects/volleyball_analytics/weights/yolov8s.pt')  # load a pretrained.py model (recommended for training)
 # https://docs.ultralytics.com/usage/cfg/#predict
+# Batched Inference:
+# https://medium.com/@smallerNdeeper/yolov8-batch-inference-implementation-using-tensorrt-2-converting-to-batch-model-engine-e02dc203fc8b
+
 # Train the model with 1 GPU.
 results = model.train(
-    data='/home/masoud/Desktop/projects/volleyball_analytics/data/processed/vb_actions_6.yaml',
+    data='/home/masoud/Desktop/projects/volleyball_analytics/data/processed/vb_actions_5.yaml',
     epochs=200,
     task='detect',
     batch=32,
-    optimizer='AdamW',
+    # optimizer='AdamW',
     seed=1368,
     cos_lr=True,
-    # lrf=,
+    lrf=0.1,
     lr0=0.0001,
     workers=16,
     imgsz=640,
