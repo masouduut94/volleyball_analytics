6cf016df085b7606c3d934f2dc5d2c630889c7e3
masoud <masouduut94@gmail.com>
2024-01-10 12:21:54 +0330

    Added yolo_split_train_test.py script to split data to train/val data.

---
+++ scripts/yolo_split_train_test.py
@@ -0,0 +1,32 @@
+from os import makedirs
+from pathlib import Path
+from os.path import join
+from random import shuffle
+from shutil import copy2, move
+
+from natsort import natsorted
+
+dataset_root = '/home/masoud/Desktop/projects/volleyball_analytics/data/preprocessed/6_classs_V2'
+new_path = '/home/masoud/Desktop/projects/volleyball_analytics/data/processed/6_classs_V2'
+all_images = natsorted(list(Path(dataset_root).rglob('*.png')), key=lambda x: x.stem)
+all_labels = natsorted(list(Path(dataset_root).rglob('*.txt')), key=lambda x: x.stem)
+pairs = list(zip(all_images, all_labels))
+
+
+test_size = 0.1
+test_length = int(len(pairs) * test_size)
+
+shuffle(pairs)
+
+for i, (img, lbl) in enumerate(pairs):
+    if i <= test_length:
+        img_path = join(new_path, 'images', 'test')
+        lbl_path = join(new_path, 'labels', 'test')
+    else:
+        img_path = join(new_path, 'images', 'train')
+        lbl_path = join(new_path, 'labels', 'train')
+    makedirs(img_path, exist_ok=True)
+    makedirs(lbl_path, exist_ok=True)
+    move(img.as_posix(), img_path)
+    move(lbl.as_posix(), lbl_path)
+
--- src/ml/yolo/train-bbox.py
+++ src/ml/yolo/train-bbox.py
@@ -1,23 +1,48 @@
+"""
+To know more about parameters:   https://docs.ultralytics.com/modes/train/#arguments
+
+- epochs	100
+- patience	50
+- imgsz	640
+- optimizer	'[SGD, Adam, Adamax, AdamW, NAdam, RAdam, RMSProp, auto]'
+- cos_lr	False	use cosine learning rate scheduler
+- close_mosaic	10	(int) disable mosaic augmentation for final epochs (0 to disable)
+- label_smoothing	0.0
+- amp	True	Automatic Mixed Precision (AMP) training, choices=[True, False]
+- profile	False	profile ONNX and TensorRT speeds during training for loggers
+- lr0	0.01	initial learning rate (i.e. SGD=1E-2, Adam=1E-3)
+- lrf	0.01	final learning rate (lr0 * lrf)
+- warmup_epochs	3.0	warmup epochs (fractions ok)
+- warmup_momentum	0.8	warmup initial momentum
+- nbs	64	nominal batch size
+- plots	False	save plots and images during train/val
+
+
+"""
+
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO('/home/masoud/Desktop/projects/volleyball_analytics/weights/yolov8s.pt')  # load a pretrained.py model (recommended for training)
+model = YOLO('/home/masoud/Desktop/projects/volleyball_analytics/weights/yolov8n.pt')  # load a pretrained.py model (recommended for training)
 # https://docs.ultralytics.com/usage/cfg/#predict
 # Batched Inference:
 # https://medium.com/@smallerNdeeper/yolov8-batch-inference-implementation-using-tensorrt-2-converting-to-batch-model-engine-e02dc203fc8b
 
 # Train the model with 1 GPU.
 results = model.train(
-    data='/home/masoud/Desktop/projects/volleyball_analytics/data/processed/vb_actions_5.yaml',
-    epochs=200,
+    data='/home/masoud/Desktop/projects/volleyball_analytics/data/processed/vb_actions_6.yaml',
+    epochs=100,
     task='detect',
     batch=32,
-    # optimizer='AdamW',
+    half=True,
+    optimizer='AdamW',
     seed=1368,
     cos_lr=True,
-    lrf=0.1,
-    lr0=0.0001,
+    lr0=0.001,
+    lrf=0.01,
     workers=16,
     imgsz=640,
-    device=[0]
+    device=[0],
+    plots=True
 )
+
