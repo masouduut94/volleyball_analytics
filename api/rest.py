b6b5a8e967dcaeb6c9d2359cb987f653f33dc00e
masoud <masouduut94@gmail.com>
2024-01-27 17:24:44 +0330

    Updated the requirements.txt

--- api/rest.py
+++ api/rest.py
@@ -1,16 +1,18 @@
 from fastapi import FastAPI
-from .models import Match
-from .schemas import MatchData
+from api.database import db_setup, Team, Nation, Player, Series, Camera, Match, Video, Rally
+from .schemas import (TeamData, NationData, PlayerData, VideoData, MatchData, ServiceData, RallyData, SeriesData,
+                      CameraData)
 
 app = FastAPI()
+mode = 'test'
+session, engine = db_setup(mode=mode)
 
 
 @app.get('/match/{match_id}')
 async def get_item(match_id: int):
-    new = Match.get(match_id)
+    new = Match.get(session, match_id)
     res = MatchData.model_validate(new).model_dump()
     return res
-
 
 # @app.get('/matches')
 # async def create(match: MatchData):
--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -1,18 +1,37 @@
-from fastapi.testclient import TestClient
-from api.rest import app
-from api.models import Match
-from api.schemas import MatchData
 import unittest
 
+from fastapi.testclient import TestClient
+from api.rest import app
+from api.database import db_setup, Base, Team, Nation, Player, Series, Camera, Match, Video, Rally
+from api.schemas import MatchData, NationData
 
-class MatchTest(unittest.TestCase):
-    def setUp(self):
-        self.client = TestClient(app)
+mode = 'test'
+client = TestClient(app)
+
+Session, engine = db_setup(mode=mode)
+Base.metadata.drop_all(bind=engine)
+Base.metadata.create_all(bind=engine)
+
+
+class TestCase(unittest.TestCase):
+    #
+    # def tearDown(self) -> None:
+    #     # Drop the tables in the test database
+    #     Base.metadata.drop_all(bind=engine)
 
     def test_get_matches(self):
-        match_id = 1
-        response = self.client.get(f"/match/{match_id}")
+        # Base.metadata.create_all(bind=engine)
+        n = NationData(name='wfwafa', display_name='USA')
+        nation = Nation(**n.model_dump())
+        # nation = Nation.save(**n.model_dump())
+        response = client.get(f"/match/{nation.id}")
         self.assertEqual(response.status_code, 200)
         self.assertIn('team1_id', dict(response.json()))
 
-    
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
