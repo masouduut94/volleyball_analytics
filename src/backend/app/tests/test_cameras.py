f562ae249380f11f1ca086ec94e8d74673c743ce
masoud <masouduut94@gmail.com>
2024-02-04 09:30:42 +0330

    Camera API, ORM, Schema tests passed:white_check_mark:

--- src/backend/app/tests/test_cameras.py
+++ src/backend/app/tests/test_cameras.py
@@ -1,17 +1,9 @@
 import unittest
 
-from src.backend.app.schemas.cameras import CameraBaseSchema
+from src.backend.app.schemas.cameras import CameraBaseSchema, CameraCreateSchema
 from src.backend.app.db.engine import Base, engine, get_db
 from fastapi.testclient import TestClient
 from src.backend.app.app import app
-
-"""
-Testing Match
-    team1_id
-    team2_id
-    series_id
-    video_id
-"""
 
 
 class CameraTest(unittest.TestCase):
@@ -23,50 +15,51 @@ class CameraTest(unittest.TestCase):
     def tearDown(self):
         Base.metadata.drop_all(bind=engine)
 
-    def test_get_one_match(self):
-        # Testing match creation and fetching for one match.
-        t = CameraBaseSchema()
-        response = self.client.post("/api/matches/", json=t.model_dump())
+    def test_get_one_camera(self):
+        # Testing camera creation and fetching for one camera.
+        t = CameraCreateSchema(angle_name='behind_team1')
+        response = self.client.post("/api/cameras/", json=t.model_dump())
         self.assertEqual(response.status_code, 201)
 
-        match_output = response.json()
-        match_output = CameraBaseSchema(**match_output)
-        response = self.client.get(f"/api/matches/{match_output.id}")
+        camera_output = response.json()
+        camera_output = CameraBaseSchema(**camera_output)
+        response = self.client.get(f"/api/cameras/{camera_output.id}")
         self.assertEqual(response.status_code, 200)
+    
+    def test_get_all_cameras(self):
+        # Testing camera creation and fetching for multiple camera.
+        t = CameraCreateSchema(angle_name='behind_team1')
+        e = CameraCreateSchema(angle_name='behind_team2')
+        response = self.client.post(f"/api/cameras/", json=e.model_dump())
+        response = self.client.post(f"/api/cameras/", json=t.model_dump())
 
-    def test_update_match(self):
-        # Testing match creation and fetching for one match.
-        t = CameraBaseSchema()
-        r = self.client.post("/api/matches/", json=t.model_dump())
-        t = CameraBaseSchema(**r.json())
-
-        t.name = 'IRAN'
-        _ = self.client.put(f"/api/matches/{t.id}", json=t.model_dump())
-        r = self.client.get(f"/api/matches/{t.id}")
-        output = r.json()
-        self.assertEqual(output['name'], t.name)
-        self.assertEqual(r.status_code, 200)
-
-    def test_delete_match(self):
-        # Testing match creation and fetching for one match.
-        t = CameraBaseSchema()
-        r = self.client.post("/api/matches/", json=t.model_dump())
-        t = CameraBaseSchema()
-
-        f = self.client.delete(f"/api/matches/{t.id}")
-        self.assertEqual(f.status_code, 200)
-
-        r = self.client.get(f"/api/matches/{t.id}")
-        self.assertEqual(r.status_code, 404)
-
-    def test_get_all_matches(self):
-        # Testing match creation and fetching for multiple match.
-        t = CameraBaseSchema()
-        e = CameraBaseSchema()
-        response = self.client.post(f"/api/matches/", json=e.model_dump())
-        response = self.client.post(f"/api/matches/", json=t.model_dump())
-
-        response = self.client.get(f"/api/matches/")
+        response = self.client.get(f"/api/cameras/")
         js = response.json()
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(js), 2)
+        
+    def test_update_camera(self):
+        # Testing camera creation and fetching for one camera.
+        t = CameraCreateSchema(angle_name='behind_team1')
+        r = self.client.post("/api/cameras/", json=t.model_dump())
+        t = CameraBaseSchema(**r.json())
+
+        t.angle_name = 'side_camera'
+        _ = self.client.put(f"/api/cameras/{t.id}", json=t.model_dump())
+        r = self.client.get(f"/api/cameras/{t.id}")
+        output = r.json()
+        self.assertEqual(output['angle_name'], t.angle_name)
+        self.assertEqual(r.status_code, 200)
+
+    def test_delete_camera(self):
+        # Testing camera creation and fetching for one camera.
+        t = CameraCreateSchema(angle_name='behind_team1')
+        r = self.client.post("/api/cameras/", json=t.model_dump())
+        t = CameraBaseSchema(**r.json())
+
+        f = self.client.delete(f"/api/cameras/{t.id}")
+        self.assertEqual(f.status_code, 200)
+
+        r = self.client.get(f"/api/cameras/{t.id}")
+        self.assertEqual(r.status_code, 404)
+
