71f924f0fcaab3481da81620bde918dd41bf804d
masoud <masouduut94@gmail.com>
2024-02-16 02:52:05 +0330

    Fixed bugs in api_interface.py get methods. :+1:

--- src/backend/app/api_interface.py
+++ src/backend/app/api_interface.py
@@ -1,4 +1,7 @@
 from urllib.parse import urljoin
+
+from os.path import join
+from pathlib import Path
 from fastapi import status
 import requests as rq
 from typing_extensions import List
@@ -7,7 +10,13 @@ from src.backend.app.schemas import cameras, matches, rallies, nations, players,
 
 
 class APIInterface:
+    """It helps to interact with the API."""
     def __init__(self, url):
+        """
+
+        Args:
+            url(str):
+        """
         self.base_url = url
         self.camera_url = urljoin(self.base_url, '/api/cameras/')
         self.match_url = urljoin(self.base_url, '/api/matches/')
@@ -17,21 +26,22 @@ class APIInterface:
         self.series_url = urljoin(self.base_url, '/api/series/')
         self.team_url = urljoin(self.base_url, '/api/teams/')
         self.video_url = urljoin(self.base_url, '/api/videos/')
-        print(f"connection status: {self.connected()}")
-
-    def connected(self) -> bool:
-        resp = rq.get(url=urljoin(self.base_url, '/api/check/'))
-        return resp.status_code == status.HTTP_200_OK
+        try:
+            r = rq.get(url=urljoin(self.base_url, '/api/check/'), timeout=0.001)
+        except:
+            print(f"Connection Failure. make sure API is up! ")
+        else:
+            print("API connection established.")
 
     def get_camera(self, camera_id: int = None) -> cameras.CameraBaseSchema | List[cameras.CameraBaseSchema]:
         if camera_id is not None:
             resp = rq.get(url=urljoin(self.camera_url, str(camera_id)))
             camera = cameras.CameraBaseSchema(**resp.json())
             return camera
-        resp = rq.get(url=urljoin(self.camera_url, str(camera_id)))
+        resp = rq.get(url=self.camera_url)
         all_cameras = []
         for c in resp.json():
-            camera = cameras.CameraBaseSchema(**c.json())
+            camera = cameras.CameraBaseSchema(**c)
             all_cameras.append(camera)
         return all_cameras
 
@@ -49,10 +59,10 @@ class APIInterface:
             resp = rq.get(url=urljoin(self.video_url, str(video_id)))
             video = videos.VideoBaseSchema(**resp.json())
             return video
-        resp = rq.get(url=urljoin(self.video_url, str(video_id)))
+        resp = rq.get(self.video_url)
         all_videos = []
         for v in resp.json():
-            video = videos.VideoBaseSchema(**v.json())
+            video = videos.VideoBaseSchema(**v)
             all_videos.append(video)
         return all_videos
 
@@ -69,10 +79,10 @@ class APIInterface:
             resp = rq.get(url=urljoin(self.player_url, str(player_id)))
             player = players.PlayerBaseSchema(**resp.json())
             return player
-        resp = rq.get(url=urljoin(self.player_url, str(player_id)))
+        resp = rq.get(self.player_url)
         all_players = []
         for p in resp.json():
-            player = players.PlayerBaseSchema(**p.json())
+            player = players.PlayerBaseSchema(**p)
             all_players.append(player)
         return all_players
 
@@ -89,10 +99,10 @@ class APIInterface:
             resp = rq.get(url=urljoin(self.team_url, str(team_id)))
             team = teams.TeamBaseSchema(**resp.json())
             return team
-        resp = rq.get(url=urljoin(self.team_url, str(team_id)))
+        resp = rq.get(url=self.team_url)
         all_teams = []
         for p in resp.json():
-            team = teams.TeamBaseSchema(**p.json())
+            team = teams.TeamBaseSchema(**p)
             all_teams.append(team)
         return all_teams
 
@@ -109,10 +119,10 @@ class APIInterface:
             resp = rq.get(url=urljoin(self.match_url, str(match_id)))
             match = matches.MatchBaseSchema(**resp.json())
             return match
-        resp = rq.get(url=urljoin(self.match_url, str(match_id)))
+        resp = rq.get(url=self.match_url)
         all_matches = []
-        for p in resp.json():
-            match = matches.MatchBaseSchema(**p.json())
+        for m in resp.json():
+            match = matches.MatchBaseSchema(**m)
             all_matches.append(match)
         return all_matches
 
@@ -129,10 +139,10 @@ class APIInterface:
             resp = rq.get(url=urljoin(self.nation_url, str(nation_id)))
             nation = nations.NationBaseSchema(**resp.json())
             return nation
-        resp = rq.get(url=urljoin(self.nation_url, str(nation_id)))
+        resp = rq.get(url=self.nation_url)
         all_nations = []
         for n in resp.json():
-            nation = nations.NationBaseSchema(**n.json())
+            nation = nations.NationBaseSchema(**n)
             all_nations.append(nation)
         return all_nations
 
@@ -149,10 +159,10 @@ class APIInterface:
             resp = rq.get(url=urljoin(self.series_url, str(series_id)))
             series_ = series.SeriesBaseSchema(**resp.json())
             return series_
-        resp = rq.get(url=urljoin(self.series_url, str(series_id)))
+        resp = rq.get(url=self.series_url)
         all_series = []
         for s in resp.json():
-            series_ = series.SeriesBaseSchema(**s.json())
+            series_ = series.SeriesBaseSchema(**s)
             all_series.append(series_)
         return all_series
 
@@ -169,22 +179,22 @@ class APIInterface:
         match = self.get_match(match_id)
         if match is None:
             return []
-        url_items = [self.match_url, str(match_id), 'rallies']
+        suffix = join(str(match_id), "rallies")
+        url = urljoin(self.match_url, suffix)
 
-        if rally_order is not None:
-            url = "/".join(url_items)
+        if rally_order is None:
             resp = rq.get(url=url)
-            rally = rallies.RallyBaseSchema(**resp.json())
-            return rally
+            all_rallies = []
+            for ra in resp.json():
+                rally = rallies.RallyBaseSchema(**ra)
+                all_rallies.append(rally)
+            return all_rallies
 
-        url_items.append(str(rally_order))
-        url = '/'.join(url_items)
+        suffix = join(suffix, str(rally_order))
+        url = urljoin(self.match_url, suffix)
         resp = rq.get(url=url)
-        all_rallies = []
-        for ra in resp.json():
-            rally = rallies.RallyBaseSchema(**ra.json())
-            all_rallies.append(rally)
-        return all_rallies
+        rally = rallies.RallyBaseSchema(**resp.json())
+        return rally
 
     def get_rally_by_id(self, rally_id: int = None) -> rallies.RallyBaseSchema:
         resp = rq.get(url=urljoin(self.rally_url, str(rally_id)))
@@ -207,5 +217,6 @@ class APIInterface:
 
 if __name__ == '__main__':
     # uvicorn src.backend.app.app:app
-    api_interface = APIInterface("http://localhost:8000")
-    api_interface.connected()
+    api = APIInterface("http://localhost:8000")
+    # p = api.get_rallies(match_id=1)
+    t = api.get_rallies(match_id=1, rally_order=2)
