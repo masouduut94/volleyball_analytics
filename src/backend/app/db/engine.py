dbf1f8975c79cf12f9e7c16a4be0ccd924a37500
masoud <masouduut94@gmail.com>
2024-03-11 01:25:35 +0330

    Added logger to engine.py initialization. 🔥

--- src/backend/app/db/engine.py
+++ src/backend/app/db/engine.py
@@ -2,8 +2,16 @@ from sqlalchemy.ext.declarative import declarative_base
 from src.backend.app.core.config import settings
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from loguru import logger
+import sys
 
-engine = create_engine(settings.get_db_uri())
+logger.add(sink=sys.stderr, format="{time:MMMM D, YYYY > HH:mm:ss!UTC} | {level} | {message}", serialize=True)
+db_uri = settings.get_db_uri()
+mode = settings.MODE
+engine = create_engine(db_uri)
+logger.info(f"The `MODE` is {mode} ")
+msg = f"The db = {settings.DEV_DB if mode == 'test' else settings.TEST_DB_URL} is selected."
+logger.success(msg)
 
 
 def get_db() -> Session:
